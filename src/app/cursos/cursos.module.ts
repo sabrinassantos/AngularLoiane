5cac1afb0154627bfc62e43b7226eb0530d8f864
Sabrina Santos da Silva <sabrina.silva@dbserver.com.br>
2019-08-20 16:20:47 -0300

    https://github.com/sabrinassantos/AngularLoiane.git

---
+++ src/app/cursos/cursos.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CursosComponent } from './cursos.component';
+
+
+
+@NgModule({
+  declarations: [CursosComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CursosModule { }
---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line: component-selector
+  selector: 'meu-primeiro-component',
+  template: `<p>meu primeiro component </p>`
+})
+export class MeuPrimeiroComponent { }
