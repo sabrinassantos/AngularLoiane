c56cee889a703947db690891833934f81332633d
Sabrina Santos da Silva <sabrina.silva@dbserver.com.br>
2019-08-21 09:51:51 -0300

    commit inicial aula06

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+  cursos: string[] = ['Java', 'Angular', 'Node.js'];
+
+  constructor() {
+    this.nomePortal = 'http://teste';
+  }
 
   ngOnInit() {
   }
