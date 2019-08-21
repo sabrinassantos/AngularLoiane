0e9f64fc77c185ee795c3bf541eff39ca4dc43ef
Sabrina Santos da Silva <sabrina.silva@dbserver.com.br>
2019-08-21 09:26:10 -0300

    Aula05Final

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { CursosComponent } from './cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 
 
 @NgModule({
-  declarations: [CursosComponent],
+  declarations: [
+    CursosComponent,
+    CursoDetalheComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    CursosComponent
   ]
 })
 export class CursosModule { }
