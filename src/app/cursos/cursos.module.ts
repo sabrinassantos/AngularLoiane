7b895d9a1ec93062c091b33de03a0f07579673a7
Sabrina Santos da Silva <sabrina.silva@dbserver.com.br>
2019-08-21 14:58:27 -0300

    injecao dependencia

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -1,9 +1,9 @@
-import { CursosService } from './cursos.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CursosComponent } from './cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosService } from './cursos.service';
 
 
 
